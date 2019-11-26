36b1676a18844eb30933d8729326de210241355a
giwi97 <49786113+giwi97@users.noreply.github.com>
2019-11-27 01:30:12 +0530

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './shared/about/about.component';
+import { CommonModule } from '@angular/common';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/about', pathMatch: 'full'},
+  {path: 'about', component: AboutComponent},
+  {path: '**', redirectTo: '/about', pathMatch: 'full'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './shared/about/about.component';
+import { CustomMaterialRoutingModule } from './custom-material-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
